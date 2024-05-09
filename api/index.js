d742ca74bd20963b38e93e0a1a47ee8cbaa032cf
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-05-09 23:44:15 +0530

    added signup functionality

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouters from './routers/user.route.js'
+import authRouters from './routers/auth.route.js'
 dotenv.config()
 const app = express()
-
+app.use(express.json())
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to database")
 }).catch((err)=>{
@@ -17,3 +18,4 @@ app.listen(3000,()=>{
 })
 
 app.use('/api/user',userRouters);
+app.use('/api/auth',authRouters)
